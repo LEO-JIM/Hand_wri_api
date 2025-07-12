9b2b0b90dac8503ff9a2a00e01a4f84cd2a849ca
LEO-JIM <lishy.leo1@gmail.com>
2025-07-12 09:59:44 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, send_file, abort
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+import os
+import uuid
+
+app = Flask(__name__)
+
+FONT_MAP = {
+    "sloppy1": "fonts/Sloppy_Hand.ttf",
+    "rush": "fonts/zai_NicolasSloppyPen.ttf"
+}
+
+
+@app.route('/write', methods=['POST'])
+def write():
+    data = request.get_json()
+    text = data.get("text", "")
+    style = data.get("style", "sloppy1")
+
+    font_path = FONT_MAP.get(style)
+    if not font_path or not os.path.exists(font_path):
+        return abort(400, "Invalid handwriting style")
+
+    font_prop = font_manager.FontProperties(fname=font_path)
+    filename = f"{uuid.uuid4()}.png"
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.text(0.05, 0.9, text, fontsize=16, fontproperties=font_prop, wrap=True)
+    ax.axis('off')
+    plt.savefig(filename, bbox_inches='tight')
+    plt.close()
+
+    return send_file(filename, mimetype='image/png')
